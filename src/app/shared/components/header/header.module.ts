eee2e3de5f9e7fc65cc291c096a72de09c8907cd
YauheniZhybryk <yzhybryk@datamola.com>
2021-04-17 23:51:53 +0300

    header

---
+++ src/app/shared/components/header/header.module.ts
@@ -0,0 +1,10 @@
+import {NgModule} from '@angular/core';
+import {HeaderComponent} from './header.component';
+
+@NgModule({
+	declarations: [HeaderComponent],
+	exports: [HeaderComponent],
+	providers: [],
+	imports: []
+})
+export class HeaderModule {}
