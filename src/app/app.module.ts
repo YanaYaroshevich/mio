a901fe175448d076d8b7be99cc2a5e8d7ba8f6f2
yyarashevich <yyaroshevich@exadel.com>
2021-04-17 23:50:47 +0300

    add image, main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {MainComponent} from './pages/main/main.component';
+import {TopComponent} from './pages/main/top/top.component';
 
 @NgModule({
-	declarations: [AppComponent],
+	declarations: [AppComponent, MainComponent, TopComponent],
 	imports: [BrowserModule, AppRoutingModule],
 	providers: [],
 	bootstrap: [AppComponent],
---
+++ src/app/pages/main/top/top.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+	selector: 'mio-top',
+	templateUrl: './top.component.html',
+	styleUrls: ['./top.component.scss'],
+})
+export class TopComponent {}
